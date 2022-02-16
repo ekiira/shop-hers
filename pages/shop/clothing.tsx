ecec5ec9ac1a170e44e4d91f5af05f0abfc766ea
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2022-02-16 22:48:21 +0100

    add clothing page template

--- pages/shop/clothing.tsx
+++ pages/shop/clothing.tsx
@@ -1,7 +1,78 @@
 import React from "react";
+import Link from "next/link";
+
+import Image from "next/image";
+import Container from "@material-ui/core/Container";
+
+import Grid from "@material-ui/core/Grid";
+
+import styles from "../../styles/shop.module.scss";
 
 const Clothing = () => {
-  return <div>clothing</div>;
+  return (
+    <div className="pt5 pb5">
+      <Container>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6} lg={3} className="pt4 pb4">
+            <div className={styles.category_content}>
+              <div className="relative">
+                <Image
+                  src="/images/clothing/shirt1.jpeg"
+                  alt="cloth"
+                  layout="fill"
+                />
+              </div>
+              <div className="mt1">
+                From <strong>N50, 000</strong>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3} className="pt4 pb4">
+            <div className={styles.category_content}>
+              <div className="relative">
+                <Image
+                  src="/images/clothing/shirt1.jpeg"
+                  alt="cloth"
+                  layout="fill"
+                />
+              </div>
+              <div className="mt1">
+                From <strong>N50, 000</strong>
+              </div>
+            </div>
+          </Grid>{" "}
+          <Grid item xs={12} md={6} lg={3} className="pt4 pb4">
+            <div className={styles.category_content}>
+              <div className="relative">
+                <Image
+                  src="/images/clothing/shirt1.jpeg"
+                  alt="cloth"
+                  layout="fill"
+                />
+              </div>
+              <div className="mt1">
+                From <strong>N50, 000</strong>
+              </div>
+            </div>
+          </Grid>{" "}
+          <Grid item xs={12} md={6} lg={3} className="pt4 pb4">
+            <div className={styles.category_content}>
+              <div className="relative">
+                <Image
+                  src="/images/clothing/shirt1.jpeg"
+                  alt="cloth"
+                  layout="fill"
+                />
+              </div>
+              <div className="mt1">
+                From <strong>N50, 000</strong>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 };
 
 export default Clothing;
